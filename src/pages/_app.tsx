607f60376c4a170516e8c718d4a6a1506d177f91
nairol <f.bock.2003@gmail.com>
2022-09-06 18:01:41 +0200

    Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App(props: AppProps) {
 				<meta name='description' content='Diese Seite ist für die Wetter abhängigen seltenen Tiere von der Island Sanctuary in Final Fantasy XIV.' />
 				<meta name='author' content='nairol203' />
 				<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
-				<link rel='shortcut icon' href='/logo.png' />
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<MantineProvider
