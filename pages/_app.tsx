c392ed086b77e1f1ff11de8b7fd80564cf0f5f11
nairol <f.bock.2003@gmail.com>
2022-09-17 15:38:52 +0200

    Favicon Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,15 +13,14 @@ export default function App(props: AppProps) {
 				<meta name='description' content='Diese Seite ist für die Wetter abhängigen seltenen Tiere von der Island Sanctuary in Final Fantasy XIV.' />
 				<meta name='author' content='nairol203' />
 				<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
-				<link rel='icon' href='/favicon.ico' />
+				<link rel='shortcut icon' href='/icon.png' />
 			</Head>
 
 			<MantineProvider
 				withGlobalStyles
 				withNormalizeCSS
 				theme={{
-					/** Put your mantine theme override here */
-					colorScheme: useColorScheme() ?? 'light',
+					colorScheme: useColorScheme(),
 				}}
 			>
 				<Component {...pageProps} />
