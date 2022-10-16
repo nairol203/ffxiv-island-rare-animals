6fde4cadbd63892690efdff05a847713cd856e35
nairol <f.bock.2003@gmail.com>
2022-10-16 13:53:09 +0200

    Updated Meta Tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,10 @@ export default function App(props: AppProps) {
 				<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
 				<link rel='shortcut icon' href='/icon.png' />
 				<meta property='og:title' content='FFXIV Island Rare Animals' />
+				<meta property='og:image' content='/logo.png' />
 				<meta property='og:type' content='website' />
 				<meta property='og:url' content='https://island.nairol.me' />
-				<meta property='og:site_name' content='nairol.me' />
+				<meta property='og:site_name' content='island.nairol.me' />
 				<meta property='og:description' content='Diese Seite ist für die Wetter abhängigen seltenen Tiere von der Island Sanctuary in Final Fantasy XIV.' />
 				<meta name='twitter:title' content='FFXIV Island Rare Animals' />
 				<meta name='twitter:description' content='Diese Seite ist für die Wetter abhängigen seltenen Tiere von der Island Sanctuary in Final Fantasy XIV.' />
